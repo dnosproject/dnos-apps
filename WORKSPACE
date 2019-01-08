84813d036d5a9638dfdc15176333f696bb8285be
adibrastegarnia <adib.rastegarnia@gmail.com>
2019-01-07 23:14:09 -0500

    sample external apps using gRPC model

---
+++ WORKSPACE
@@ -0,0 +1,88 @@
+workspace(name = "org_dnosproject_dnos_apps")
+
+#### Docker related repos
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+# Download the rules_docker repository at release v0.6.0
+http_archive(
+    name = "io_bazel_rules_docker",
+    sha256 = "c0e9d27e6ca307e4ac0122d3dd1df001b9824373fb6fb8627cd2371068e51fef",
+    strip_prefix = "rules_docker-0.6.0",
+    urls = ["https://github.com/bazelbuild/rules_docker/archive/v0.6.0.tar.gz"],
+)
+
+# OPTIONAL: Call this to override the default docker toolchain configuration.
+# This call should be placed BEFORE the call to "container_repositories" below
+# to actually override the default toolchain configuration.
+# Note this is only required if you actually want to call
+# docker_toolchain_configure with a custom attr; please read the toolchains
+# docs in /toolchains/docker/ before blindly adding this to your WORKSPACE.
+
+load(
+    "@io_bazel_rules_docker//toolchains/docker:toolchain.bzl",
+    docker_toolchain_configure = "toolchain_configure",
+)
+
+docker_toolchain_configure(
+    name = "docker_config",
+    # OPTIONAL: Path to a directory which has a custom docker client config.json.
+    # See https://docs.docker.com/engine/reference/commandline/cli/#configuration-files
+    # for more details.
+    client_config = "/path/to/docker/client/config",
+)
+
+load(
+    "@io_bazel_rules_docker//container:container.bzl",
+    "container_pull",
+    container_repositories = "repositories",
+)
+
+# This is NOT needed when going through the language lang_image
+# "repositories" function(s).
+container_repositories()
+
+container_pull(
+    name = "java_base",
+    # 'tag' is also supported, but digest is encouraged for reproducibility.
+    #digest = "sha256:deadbeef",
+    registry = "gcr.io",
+    repository = "distroless/java",
+)
+
+load(
+    "@io_bazel_rules_docker//java:image.bzl",
+    _java_image_repos = "repositories",
+)
+
+_java_image_repos()
+
+git_repository(
+    name = "dnos_core",
+    commit = "43e58213fadc0197195c99eb418d89b305a543ed",
+    remote = "https://bitbucket.org/dnosproject/dnos-core/src/master",
+)
+
+git_repository(
+    name = "dnos_services",
+    commit = "0caf5f2265b9e2461af90d42a2ab5009a7178a89",
+    remote = "https://bitbucket.org/dnosproject/dnos-services/src/master",
+)
+
+git_repository(
+    name = "dnos_core_grpc",
+    commit = "70a8516dd894715cae57c13c22a94998a3a58aeb",
+    remote = "https://bitbucket.org/dnosproject/dnos-core-grpc/src/master",
+)
+
+load("@dnos_core//tools/build/bazel:generate_workspace.bzl", "generated_maven_jars")
+
+generated_maven_jars()
+
+load("@dnos_core//tools/build/bazel:grpc_workspace.bzl", "generate_grpc")
+
+generate_grpc()
+
+load("@io_grpc_grpc_java//:repositories.bzl", "grpc_java_repositories")
+
+grpc_java_repositories()
---
+++ linkfailuredetector/linkfailuredetector.java
@@ -0,0 +1,133 @@
+package linkfailuredetector;
+
+import api.topostore.TopoEdge;
+import api.topostore.TopoEdgeType;
+import api.topostore.TopoSwitch;
+import config.ConfigService;
+import drivers.controller.Controller;
+import drivers.onos.OnosController;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.apache.log4j.Logger;
+import org.onosproject.grpc.net.models.EventNotificationGrpc;
+import org.onosproject.grpc.net.models.ServicesProto;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Set;
+
+public class linkfailuredetector {
+  private static Logger log = Logger.getLogger(linkfailuredetector.class);
+
+  static String serverId = null;
+  static String clientId = "linkfailuredetector";
+
+
+
+  public static void main(String[] args) {
+
+
+    ManagedChannel channel;
+    String controllerName;
+    String controllerIP;
+    String grpcPort;
+    Controller controller = null;
+
+    ConfigService configService = new ConfigService();
+    configService.init();
+
+    controllerName = configService.getConfig().getControllerName();
+    controllerIP = configService.getConfig().getControllerIp();
+
+    grpcPort = configService.getConfig().getGrpcPort();
+    controller = new OnosController();
+    Controller finalController = controller;
+    Set<TopoSwitch> topoSwitches = finalController.topoStore.getSwitches();
+
+    Set<TopoEdge> topoEdges = finalController.topoStore.getTopoEdges();
+    EventNotificationGrpc.EventNotificationStub linkEventNotification;
+
+    channel =
+        ManagedChannelBuilder.forAddress(controllerIP, Integer.parseInt(grpcPort))
+            .usePlaintext()
+            .build();
+
+    linkEventNotification = EventNotificationGrpc.newStub(channel);
+
+    ServicesProto.RegistrationRequest request =
+        ServicesProto.RegistrationRequest.newBuilder().setClientId(clientId).build();
+    linkEventNotification.register(
+        request,
+        new StreamObserver<ServicesProto.RegistrationResponse>() {
+          @Override
+          public void onNext(ServicesProto.RegistrationResponse value) {
+
+            serverId = value.getServerId();
+          }
+
+          @Override
+          public void onError(Throwable t) {}
+
+          @Override
+          public void onCompleted() {}
+        });
+
+    ServicesProto.Topic topic =
+        ServicesProto.Topic.newBuilder()
+            .setClientId(clientId)
+            .setType(ServicesProto.topicType.LINK_EVENT)
+            .build();
+
+    class Event implements Runnable {
+
+      @Override
+      public void run() {
+
+        linkEventNotification.onEvent(
+            topic,
+            new StreamObserver<ServicesProto.Notification>() {
+              @Override
+              public void onNext(ServicesProto.Notification value) {
+
+                TopoEdge edge = new TopoEdge();
+                edge.setSrc(value.getLinkEvent().getLink().getSrc().getDeviceId());
+                edge.setSrcPort(value.getLinkEvent().getLink().getSrc().getPortNumber());
+                edge.setDst(value.getLinkEvent().getLink().getDst().getDeviceId());
+                edge.setDstPort(value.getLinkEvent().getLink().getDst().getPortNumber());
+                edge.setType(TopoEdgeType.SWITCH_SWITCH);
+
+                switch (value.getLinkEvent().getLinkEventType()) {
+                  case LINK_ADDED:
+                    log.info("LINK_ADDED");
+                    finalController.topoStore.addEdge(edge);
+
+                    break;
+                  case LINK_REMOVED:
+                    log.info("LINK_REMOVED");
+                    finalController.topoStore.removeEdge(edge);
+                    break;
+                  case LINK_UPDATED:
+                    //log.info("LINK_UPDATED");
+                }
+
+                //log.info(finalController.topoStore.getTopoEdges().size());
+              }
+
+              @Override
+              public void onError(Throwable t) {}
+
+              @Override
+              public void onCompleted() {}
+            });
+
+        while (true) {}
+      }
+    }
+
+    Event event = new Event();
+    Thread t = new Thread(event);
+    t.start();
+  }
+}
